ad7be8cfc6959f175be92981651dcf7a5800eee3
012MykA <1deadeye1@mai.ru>
2026-06-27 11:10:25 +0700

    camera pos

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,29 @@ int main()
     rt::HittableList world;
 
     auto materialGround = std::make_shared<rt::Lambertian>(glm::vec3(0.8f, 0.8f, 0.0f));
-    auto materialCenter = std::make_shared<rt::Lambertian>(glm::vec3(0.1, 0.2, 0.5));
+    auto materialCenter = std::make_shared<rt::Lambertian>(glm::vec3(0.1f, 0.2f, 0.5f));
     auto materialLeft = std::make_shared<rt::Metal>(glm::vec3(0.8f, 0.8f, 0.8f), 0.3f);
     auto materialRight = std::make_shared<rt::Metal>(glm::vec3(0.8f, 0.6f, 0.2f), 1.0f);
+    auto materialUp = std::make_shared<rt::Metal>(glm::vec3(0.9f, 0.9f, 0.9f), 0.1f);
 
     world.Add(std::make_shared<rt::Sphere>(glm::vec3(0.0f, -100.5f, -1.0f), 100.0f, materialGround));
-    world.Add(std::make_shared<rt::Sphere>(glm::vec3(0.0f, 0.0f, -1.5f), 0.5f, materialCenter));
-    world.Add(std::make_shared<rt::Sphere>(glm::vec3(-1.0f, 0.0f, -1.0f), 0.5f, materialLeft));
-    world.Add(std::make_shared<rt::Sphere>(glm::vec3(1.0f, 0.0f, -1.0f), 0.5f, materialRight));
+    world.Add(std::make_shared<rt::Sphere>(glm::vec3(0.0f, 0.0f, -0.5f), 0.5f, materialCenter));
+    world.Add(std::make_shared<rt::Sphere>(glm::vec3(-1.0f, 0.0f, 0.0f), 0.5f, materialLeft));
+    world.Add(std::make_shared<rt::Sphere>(glm::vec3(1.0f, 0.0f, 0.0f), 0.5f, materialRight));
+    world.Add(std::make_shared<rt::Sphere>(glm::vec3(0.0f, 1.0f, 0.0f), 0.5f, materialUp));
 
     rayTracer.SetWorld(world);
 
     rt::RenderConfig renderConfig{
-        .samplesPerPixel = 100000,
-        .maxDepth = 1000,
+        .samplesPerPixel = 100,
+        .maxDepth = 100,
     };
     rayTracer.SetRenderConfig(renderConfig);
+
+    rt::Camera cam;
+    cam.cameraCenter = {0.0f, 0.0f, 1.2f};
+
+    rayTracer.SetCamera(cam);
 
     rayTracer.Render("images/result.png");
 }
